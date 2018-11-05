a13bd47683822fc79fe6467114636bb3ec6270bb
Neil Tiedeman <neilt@knowledgeservices.com>
2018-11-04 20:17:59 -0500

    initial commit

--- BowlingGameTests/BowlingGameTests.cs
+++ BowlingGameTests/BowlingGameTests.cs
@@ -7,7 +7,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace UnitTestProject1
 {
     [TestClass]
-    public class BowlingTests2
+    public class BowlingGameTests
     {
         private FrameFactory _factory = new ConcreteFrameFactory();
       
